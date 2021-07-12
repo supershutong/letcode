6fea854d17eb09cbf222baec2c3c680cbb6d2bf9
liuxiangyang <liuxiangyang@cslc.com.cn>
2021-07-12 15:48:39 +0800

    15. 三数之和

--- 15. 三数之和.js
+++ 15. 三数之和.js
@@ -3,26 +3,28 @@
  * @return {number[][]}
  */
 var threeSum = function (nums) {
-  nums.sort((a, b) => a - b)
+  nums.sort((a, b) => a - b) // 排序
   let res = []
   for (let i = 0, len = nums.length; i < len; i++) {
-    if (nums[i] > 0) break
-    if (nums[i] === nums[i - 1]) continue
-    let L = i + 1, R = len - 1;
+    if (nums[i] > 0) break // 若当前数大于0，则三数之和一定大于0，所以结束循环
+    if (nums[i] === nums[i - 1]) continue // 去重
+    let L = i + 1,
+      R = len - 1
     while (L < R) {
       let sum = nums[i] + nums[L] + nums[R]
       if (sum === 0) {
         res.push([nums[i], nums[L], nums[R]])
-        while (nums[L] === nums[++L]) continue
+        while (nums[L] === nums[++L]) continue // 去重
       } else if (sum > 0) {
-        while (nums[R] === nums[--R]) continue
-      } else if (sum < 0) {
-        while (nums[L] === nums[++L]) continue
+        while (nums[R] === nums[--R]) continue // 去重
+      } else if (sum <= 0) {
+        while (nums[L] === nums[++L]) continue // 去重
       }
     }
   }
   return res
-};
+}
 
 var nums = [-1, 2, -2, -1, 1, 0]
-threeSum(nums)
+let result = threeSum(nums)
+console.log(result)
