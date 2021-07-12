9bb8fce5bb7ed56eeab683428606da4c09b63f5f
liuxiangyang <liuxiangyang@cslc.com.cn>
2021-07-12 17:41:59 +0800

    415. 字符串相加（大数相加）

--- 415. 字符串相加（大数相加）.js
+++ 415. 字符串相加（大数相加）.js
@@ -5,32 +5,39 @@
  */
 // 方法一 补0后按位求和，遍历更新剩下字符串
 var addStrings = function (num1, num2) {
-  let sumStr = '', temp, exceed = 0, i = Math.max(num1.length, num2.length)
+  let i = Math.max(num1.length, num2.length)
+  num1 = num1.padStart(i, '0') // 补齐
   num2 = num2.padStart(i, '0')
-  num1 = num1.padStart(i, '0')
-
+  let temp,
+    exceed = 0,
+    sum = ''
   while (i--) {
-    temp = (+(num1[i]) + +(num2[i]) + exceed)
+    // 逐位计算
+    temp = +num1[i] + +num2[i] + exceed
+    exceed = temp >= 10 ? 1 : 0
+    sum = (exceed ? temp - 10 : temp) + sum
     num1 = num1.slice(0, -1)
     num2 = num2.slice(0, -1)
-    sumStr = (temp - 10 * (exceed = temp > 9 ? 1 : 0)) + sumStr
   }
-  return exceed ? 1 + sumStr : sumStr
-};
+  return exceed ? 1 + sum : sum // 最后一次进位
+}
 
 // 方法二 遍历更新num2
-var addStrings = function (num1, num2) {
-  let a = num1.length, b = num2.length, exceed = 0, result = ''
+var addStrings1 = function (num1, num2) {
+  let a = num1.length,
+    b = num2.length,
+    exceed = 0,
+    result = ''
   while (a || b) {
-    a ? exceed += +num1[--a] : ''
-    b ? exceed += +num2[--b] : ''
-    result = exceed % 10 + result
+    a ? (exceed += +num1[--a]) : ''
+    b ? (exceed += +num2[--b]) : ''
+    result = (exceed % 10) + result
     exceed = exceed > 9 ? 1 : 0
   }
   return exceed ? 1 + result : result
 }
 
-
-var num1 = "99", num2 = "9"
+var num1 = '99',
+  num2 = '9'
 var res = addStrings(num1, num2)
 console.log(res)
