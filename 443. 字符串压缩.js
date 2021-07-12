bdf4ee6f1bf2d5dd4b934a3bb14f01477a17c679
liuxiangyang <liuxiangyang@cslc.com.cn>
2021-07-12 19:21:02 +0800

    压缩字符串

---
+++ 443. 字符串压缩.js
@@ -0,0 +1,20 @@
+/**
+ * 利用字符重复出现的次数，编写一种方法，实现基本的字符串压缩功能。
+ * 如：字符串aabcccccaaa会变为a2b1c5a3。若“压缩”后的字符串没有变短，则返回原先的字符串。
+ */
+var compressString = function (str) {
+  let res = '',
+    time = 1
+  for (let i = 1; i <= str.length; i++) {
+    if (str[i] === str[i - 1]) {
+      time++
+    } else {
+      res = res + str[i - 1] + time
+      time = 1
+    }
+  }
+  return res
+}
+
+let str = 'aabcccccaaa'
+console.log(compressString(str)) // 'a2b1c5a3'
