ddf4265b783125529c9222b6c34caa747a4e3fe8
liuxiangyang <liuxiangyang@cslc.com.cn>
2021-07-12 14:56:21 +0800

    110. 平衡二叉树

---
+++ 110. 平衡二叉树.js
@@ -0,0 +1,32 @@
+/**
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+    本题中，一棵高度平衡二叉树定义为：
+    一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ * 时间复杂度 O(n)，空间复杂度 O(n)
+ */
+var isBalanced = function (root) {
+  return balanced(root) !== -1
+}
+
+function balanced(node) {
+  if (!node) return 0
+  const left = balanced(node.left)
+  const right = balanced(node.right)
+  if (left === -1 || right === -1 || Math.abs(left - right) > 1) {
+    return -1
+  }
+  return Math.max(left, right) + 1
+}
